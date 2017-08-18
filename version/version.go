ff3841d3b6adafb172e8dc1816315a421c29bd82
Joseph Schorr <jschorr@gmail.com>
2017-08-18 19:58:40 -0400

    Add versioning support to the language server

---
+++ version/version.go
@@ -0,0 +1,11 @@
+// Copyright 2017 The Serulian Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package version defines the versioning information for the language server.
+package version
+
+var (
+	// GitSHA is the GIT SHA of this version of the language server. Will be empty when developing.
+	GitSHA = ""
+)
